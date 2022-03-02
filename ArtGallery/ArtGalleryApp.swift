59c85a3b47e8c948ad74893ee86ce76972f0b898
Jaleel <jaleeldeangalo@gmail.com>
2022-03-01 19:00:33 -0600

    Initial Commit

---
+++ ArtGallery/ArtGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtGalleryApp.swift
+//  ArtGallery
+//
+//  Created by Jaleel Gilbert on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
